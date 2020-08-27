1f520bea7eb62bde65dcafcf1c569b310b4c4977
Thao Ma <MaThao@gmail.com>
2020-08-26 20:23:26 -0600

    update descriptor function and data.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+var descriptors = require('./data.js').descriptors
+
 // Create variables targetting the relevant DOM elements here 👇
 
 
@@ -16,4 +18,10 @@ var currentCover;
 // We've provided one function to get you started
 function getRandomIndex(array) {
   return Math.floor(Math.random() * array.length);
+}
+
+function getDescriptor() {
+// take 1 descriptor from the array
+  var index = getRandomIndex(descriptors);
+  return descriptors[index]
 }
