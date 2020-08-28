49d499241a9d03d0b59d2fbb35cb69b61b924e4b
Peyton Jones <peytonj.dev@gmail.com>
2020-08-28 10:34:53 -0600

    finish iteration 1

--- src/main.js
+++ src/main.js
@@ -4,35 +4,38 @@ var randomImg = document.querySelector('.cover-image')
 var descriptorOne = document.querySelector('.tagline-1')
 var descriptorTwo = document.querySelector('.tagline-2')
 
+var ranCoverButton = document.querySelector('.random-cover-button');
+
+ranCoverButton.addEventListener("click", generateRandomCover);
+
+
 // On page load generate random values
-descriptorOne.innerText = getDescriptor();
-descriptorTwo.innerText = getDescriptor();
-randomImg.src = getRandomImage();
-randomCoverTitle.innerText = getRandomTitle()
+generateRandomCover();
 
 
 // Create variables targetting the relevant DOM elements here 👇
 
 // We've provided a few variables below
 var savedCovers = [
-  new Cover("http://3.bp.blogspot.com/-iE4p9grvfpQ/VSfZT0vH2UI/AAAAAAAANq8/wwQZssi-V5g/s1600/Do%2BNot%2BForsake%2BMe%2B-%2BImage.jpg", "Sunsets and Sorrows", "sunsets", "sorrows")
 ];
 
-var currentCover;
+var currentCover = new Cover(getRandomImage(),getRandomTitle(),getDescriptor(),getDescriptor())
+
 
 
 // Add your event listeners here 👇
-
-
 // Create your event handlers and other functions here 👇
+function generateRandomCover(){
+  var currentCover = new Cover(getRandomImage(),getRandomTitle(),getDescriptor(),getDescriptor())
+
+  descriptorOne.innerText = currentCover.tagline1;
+  descriptorTwo.innerText = currentCover.tagline2;
+  randomImg.src = currentCover.cover;
+  randomCoverTitle.innerText = currentCover.title;
+}
 
 
-// We've provided one function to get you started
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-};
 
-//Get random image
 function getRandomImage(){
   var randomImageIndex = getRandomIndex(covers);
   var randomImage = covers[randomImageIndex];
@@ -40,7 +43,7 @@ function getRandomImage(){
   return randomImage;
 };
 
-//Get random titles
+
 function getRandomTitle(){
   var randomTitleIndex = getRandomIndex(titles);
   var randomTitle = titles[randomTitleIndex];
@@ -51,4 +54,9 @@ function getRandomTitle(){
 function getDescriptor() {
   var index = getRandomIndex(descriptors)
   return descriptors[index]
+};
+
+// We've provided one function to get you started
+function getRandomIndex(array) {
+  return Math.floor(Math.random() * array.length);
 };
