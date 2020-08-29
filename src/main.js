eb68f32cccb1999c3a75fd4da0540a4f6560857f
Thao Ma <MaThao@gmail.com>
2020-08-28 21:13:50 -0600

    Update to show form view and hide home page.

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
 // Create variables targetting the relevant DOM elements here 👇
-var randomCoverTitle = document.querySelector('.cover-title')
-var randomImg = document.querySelector('.cover-image')
-var descriptorOne = document.querySelector('.tagline-1')
-var descriptorTwo = document.querySelector('.tagline-2')
-
+var randomCoverTitle = document.querySelector('.cover-title');
+var randomImg = document.querySelector('.cover-image');
+var descriptorOne = document.querySelector('.tagline-1');
+var descriptorTwo = document.querySelector('.tagline-2');
 var ranCoverButton = document.querySelector('.random-cover-button');
-
-ranCoverButton.addEventListener("click", generateRandomCover);
+var moveToFormButton = document.querySelector('.make-new-button');
+var coverForm = document.querySelector('.form-view')
+var homePage = document.querySelector('.home-view')
 
 
 // On page load generate random values
 generateRandomCover();
-
-
-// Create variables targetting the relevant DOM elements here 👇
 
 // We've provided a few variables below
 var savedCovers = [
@@ -21,10 +18,29 @@ var savedCovers = [
 
 var currentCover = new Cover(getRandomImage(),getRandomTitle(),getDescriptor(),getDescriptor())
 
-
-
 // Add your event listeners here 👇
+ranCoverButton.addEventListener("click", generateRandomCover);
+moveToFormButton.addEventListener("click", displayHiddenForm);
+// moveToFormButton.addEventListener("click", hideHomeView);
+
 // Create your event handlers and other functions here 👇
+//
+// function hideHomeView() {
+  // homePage.style.display = 'none'
+// }
+
+function displayHiddenForm() {
+
+  if (coverForm.style.display === '') {
+    coverForm.style.display = 'block'
+    homePage.style.display = 'none'
+  } else {
+
+  }
+  console.log(coverForm.style.display);
+};
+
+
 function generateRandomCover(){
   var currentCover = new Cover(getRandomImage(),getRandomTitle(),getDescriptor(),getDescriptor())
 
@@ -32,9 +48,7 @@ function generateRandomCover(){
   descriptorTwo.innerText = currentCover.tagline2;
   randomImg.src = currentCover.cover;
   randomCoverTitle.innerText = currentCover.title;
-}
-
-
+};
 
 function getRandomImage(){
   var randomImageIndex = getRandomIndex(covers);
@@ -42,7 +56,6 @@ function getRandomImage(){
 
   return randomImage;
 };
-
 
 function getRandomTitle(){
   var randomTitleIndex = getRandomIndex(titles);
