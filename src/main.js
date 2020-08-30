6a3283306d55cf77109d6be09f408b2b593a335b
Peyton Jones <peytonj.dev@gmail.com>
2020-08-30 10:16:39 -0600

    view saved cover page completed

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ var randomCoverTitle = document.querySelector('.cover-title')
 var randomImg = document.querySelector('.cover-image')
 var descriptorOne = document.querySelector('.tagline-1')
 var descriptorTwo = document.querySelector('.tagline-2')
-
 var ranCoverButton = document.querySelector('.random-cover-button');
 
 ranCoverButton.addEventListener("click", generateRandomCover);
@@ -14,7 +13,6 @@ generateRandomCover();
 
 
 // Create variables targetting the relevant DOM elements here 👇
-
 // We've provided a few variables below
 var savedCovers = [
 ];
@@ -25,6 +23,40 @@ var currentCover = new Cover(getRandomImage(),getRandomTitle(),getDescriptor(),g
 
 // Add your event listeners here 👇
 // Create your event handlers and other functions here 👇
+var homeBtn = document.querySelector(".home-button")
+var saveCoverBtn = document.querySelector(".save-cover-button")
+var viewSavedCoversBtn = document.querySelector(".view-saved-button")
+var savedPage = document.querySelector(".saved-view")
+var homePage = document.querySelector('.home-view')
+
+viewSavedCoversBtn.addEventListener("click", viewSavedCovers);
+homeBtn.addEventListener("click", viewHomePage);
+
+function viewSavedCovers(){
+  homeBtn.classList.remove("hidden")
+  savedPage.classList.remove("hidden")
+  homePage.classList.add("hidden")
+  ranCoverButton.classList.add("hidden")
+  saveCoverBtn.classList.add("hidden")
+}
+
+function viewHomePage(){
+  homeBtn.classList.add("hidden")
+  savedPage.classList.add("hidden")
+  homePage.classList.remove("hidden")
+  ranCoverButton.classList.remove("hidden")
+  saveCoverBtn.classList.remove("hidden")
+}
+
+
+
+
+
+
+
+
+
+
 function generateRandomCover(){
   var currentCover = new Cover(getRandomImage(),getRandomTitle(),getDescriptor(),getDescriptor())
 
